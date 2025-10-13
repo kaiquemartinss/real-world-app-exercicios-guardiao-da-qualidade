150d17cde73764b29070d5f9ada62535da4d0657
Kaique Martins <kaique.mds@outlook.com>
2025-10-13 20:49:01 -0300

    Adicionando automação de login com sucesso e login com credenciais incorretas

--- exercicio01/automacao/cypress/e2e/login.cy.js
+++ exercicio01/automacao/cypress/e2e/login.cy.js
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
+import userData from '../fixtures/userData.json'
+
+describe('Login com sucesso', () => {
+  it('Deve fazer login com um usuário válido', () => {
     cy.visit('http://localhost:3000/signin')
+    cy.get("[data-test='signin-username']").type(userData.userSuccess.username)
+    cy.get("[data-test='signin-password']").type(userData.userSuccess.password)
+    cy.get("[data-test='signin-submit']").click()
+    cy.get("[data-test='main']").contains('Public')
+  })
+
+  it('Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas', () => {
+    cy.visit('http://localhost:3000/signin')
+    cy.get("[data-test='signin-username']").type(userData.userFail.username)
+    cy.get("[data-test='signin-password']").type(userData.userFail.password)
+    cy.get("[data-test='signin-submit']").click()
+    cy.get("[data-test='signin-error']").contains('Username or password is invalid')
   })
 })
